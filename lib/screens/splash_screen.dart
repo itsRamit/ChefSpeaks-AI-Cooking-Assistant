98fe047b1659c74edbdf56b5537eada0dc334d26
itsRamit <ece22145@iiitkalyani.ac.in>
2025-07-05 18:59:01 +0530

    feat : stored user details in local using SharedPreferences

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'package:chefspeaks/utils/shared_prefs_keys.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -20,7 +22,6 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    // Show login button after splash delay
     Timer(const Duration(seconds: 2), () {
       if (mounted) {
         setState(() {
@@ -30,15 +31,26 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
-  void _onAuthSuccess(Session session) {
+  void _onAuthError(Object error) {
+    // Handle authentication error
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Authentication failed: ${error}')),
+    );
+  }
+
+  void _onAuthSuccess(Session session) async {
+    final user = session.user;
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(SharedPrefsKeys.isLoggedIn, true);
+    await prefs.setString(SharedPrefsKeys.userId, user.id);
+    await prefs.setString(SharedPrefsKeys.email, user.email ?? 'N/A');
+    await prefs.setString(SharedPrefsKeys.name, user.userMetadata?['full_name'] ?? 'N/A');
+    await prefs.setString(SharedPrefsKeys.contact, user.phone ?? 'N/A');
+
     Navigator.pushReplacementNamed(context, '/home');
   }
 
-  void _onAuthError(Object error) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Login failed: $error")),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
--- lib/utils/shared_prefs_keys.dart
+++ lib/utils/shared_prefs_keys.dart
@@ -1,3 +1,7 @@
 class SharedPrefsKeys {
   static const String isLoggedIn = "is_logged_in";
+  static const String userId = "user_id";
+  static const String email = "email";
+  static const String name = "name";
+  static const String contact = "contact";
 }
